bba872ca9fcfbf942c509d1c070d43e3ab955677
Saikat Sikder <45892103+mdhsaikats@users.noreply.github.com>
2024-04-23 04:41:36 +0600

    Update inventory_management_system.c

--- inventory_management_system.c
+++ inventory_management_system.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 struct inventory
 {
     char name[50];
-    float money;
+    int quantity; // Quantity of the product
     struct inventory *next;
 };
 struct inventory *top = NULL;
 
-void push(char pname[], float pmoney)
+void push(char pname[], int pquantity)
 {
     struct inventory *newNode = (struct inventory*)malloc(sizeof(struct inventory));
     if(newNode == NULL) {
@@ -17,10 +18,11 @@ void push(char pname[], float pmoney)
         return;
     }
     strcpy(newNode->name, pname);
-    newNode->money = pmoney;
+    newNode->quantity = pquantity;
     newNode->next = top;
     top = newNode;
 }
+
 void showinventory()
 {
     struct inventory *ptr = top;
@@ -28,40 +30,129 @@ void showinventory()
     printf("List of products:\n");
     while (ptr != NULL)
     {
-        printf("Product name: %s . Product Price: %f \n",ptr->name, ptr->money);
-        ptr= ptr->next;
+        printf("Product name: %s . Quantity: %d\n", ptr->name, ptr->quantity);
+        ptr = ptr->next;
     }
-    
 }
 
 void deadstock()
 {
-    
+    struct inventory *ptr = top;
+    int deadStockFound = 0;
+
+    printf("Dead Stock:\n");
+    while (ptr != NULL)
+    {
+        if (ptr->quantity == 0)
+        {
+            printf("Product name: %s\n", ptr->name);
+            deadStockFound = 1;
+        }
+        ptr = ptr->next;
+    }
+
+    if (!deadStockFound)
+    {
+        printf("No dead stock found.\n");
+    }
 }
+
+void sell(char pname[], int sellQuantity)
+{
+    struct inventory *ptr = top;
+
+    while (ptr != NULL)
+    {
+        if (strcmp(ptr->name, pname) == 0)
+        {
+            if (ptr->quantity >= sellQuantity)
+            {
+                ptr->quantity -= sellQuantity;
+                printf("Sold %d units of %s\n", sellQuantity, pname);
+                return;
+            }
+            else
+            {
+                printf("Not enough quantity of %s in stock.\n", pname);
+                return;
+            }
+        }
+        ptr = ptr->next;
+    }
+    printf("Product not found in inventory.\n");
+}
+
+void deleteProduct(char pname[])
+{
+    struct inventory *prev = NULL;
+    struct inventory *curr = top;
+
+    while (curr != NULL)
+    {
+        if (strcmp(curr->name, pname) == 0)
+        {
+            if (prev != NULL)
+            {
+                prev->next = curr->next;
+            }
+            else
+            {
+                top = curr->next;
+            }
+            free(curr);
+            printf("Product %s deleted from inventory.\n", pname);
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+    printf("Product not found in inventory.\n");
+}
+
 int main()
 {
     int option;
     char pname[50];
-    float pmoney;
+    int pquantity;
+    int sellQuantity;
+
     while (1)
     {
-        printf("Enter your option:\n1 for add your product \n2 for show the list\n");
-        scanf("%d",&option);
-        
+        printf("Enter your option:\n1 for add your product\n2 for show the list\n3 for dead stock\n4 for sell\n5 for delete product\n");
+        scanf("%d", &option);
+
         switch (option)
         {
         case 1:
-                printf("Enter product name: ");
-                scanf("%s", pname);
-                printf("Enter product price: ");
-                scanf("%f", &pmoney);
-                push(pname, pmoney);
+            printf("Enter product name: ");
+            scanf("%s", pname);
+            printf("Enter product quantity: ");
+            scanf("%d", &pquantity);
+            push(pname, pquantity);
             break;
-            
+
         case 2:
             showinventory();
-            break;    
-        
+            break;
+
+        case 3:
+            deadstock();
+            break;
+
+        case 4:
+            printf("Enter product name to sell: ");
+            scanf("%s", pname);
+            printf("Enter quantity to sell: ");
+            scanf("%d", &sellQuantity);
+            sell(pname, sellQuantity);
+            break;
+
+        case 5:
+            printf("Enter product name to delete: ");
+            scanf("%s", pname);
+            deleteProduct(pname);
+            break;
+
         default:
             break;
         }
